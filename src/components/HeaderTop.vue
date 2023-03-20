2e0fc29e4862569ff7838e9c43fc86d25fca943b
leocastroz <castroz.dev@gmail.com>
2023-03-19 23:42:27 -0300

    Fix: improve code and removing bad code.

--- src/components/HeaderTop.vue
+++ src/components/HeaderTop.vue
@@ -1,18 +1,35 @@
-<script setup>
-
-
-defineProps({
-  msg: String
-})
-
-
+<script>
+export default {
+    setup () {
+        return {
+          name: 'leonardo',
+        }
+    },
+  methods: {
+    playSound() {
+      this.$refs.sound.play();
+    }
+  }
+}
 </script>
-
 <template>
 
 <header>
- <img src="https://user-images.githubusercontent.com/72839343/181359023-213c387b-60b8-408c-9814-6da726ffc21a.png" alt="">
-  <p>Leonardo S. Castro ☕</p>
+ <img src="https://avatars.githubusercontent.com/u/72839343?v=4" alt="profile github">
+  <p>Leonardo Castro <span>" Frontend Developer "</span></p>
+
+  <div class="events" id="touch_mouse">
+    <div id="show" class="alert" name="alert">
+      <p>
+        Clique no icone para escutar o audio
+      </p>
+    </div>
+    <span @click="playSound" id="sound" class="material-symbols-outlined">volume_up</span>
+    <audio ref="sound">
+      <source src="../assets/Leonardo Castro.m4a" type="audio/mp4">
+    </audio>
+  </div> 
+
   <a href="https://github.com/leocastroz">Github</a>
   <a href="linkedin.com/in/leoscastro">Linkedin</a>
   <a href="https://www.instagram.com/leocastro_x/">Instagram</a>
@@ -29,6 +46,29 @@ defineProps({
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
 
+#show {
+  display: none;
+}
+
+#touch_mouse:hover #show {
+  display: block;
+}
+
+.alert {
+  margin-left: -50px;
+  margin-top: 20px;
+  background-color: #2e2e2e;
+  position: absolute;
+  width: 150px;
+  padding: 5px;
+  border-radius: 5px;
+}
+
+.alert p{
+  font-size: 10px;
+  color: #bdfb60;
+}
+
 *{
   margin: 0;
   padding: 0;
@@ -36,42 +76,64 @@ defineProps({
 }
 
 header{
-  width: 100vw;
+  background-image: url('https://cutewallpaper.org/27/black-out-background-wallpaper/1011426618.jpg');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
   height: 100vh;
   display: grid;
-  background: rgb(0,0,0);
-background: linear-gradient(180deg, rgba(0,0,0,1) 0%, rgb(28, 28, 28) 100%);
+  width: 100vw;
 }
 
 header img{
-  width: 200px;
+  box-shadow: 0px 0px 100px #272727;
+  margin: 30px auto 0 auto;
   border-radius: 100%;
-  margin: 40px auto 0 auto;
-  box-shadow: 0px 0px 100px #39f79b;
+  width: 200px;
 }
 
 header p{
-  text-align: center;
-  font-size: 22px;
-  font-weight: 600;
-  color: #fff;
   Font-family: 'Poppins', sans-serif;
+  text-align: center;
+  font-weight: 600;
+  font-size: 22px;
+  color: #fff;
 }
 
 header a{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-family: 'Poppins', sans-serif;
-    font-weight: 300;
-    width: 180px;
-    height: 45px;
-    text-decoration: none;
-    background: -webkit-linear-gradient(#00FF85, #FCFF7E);
-    margin: 0 auto;
-    color: rgb(0, 0, 0);
-    font-size: 14px;
-    border-radius: 100px;
+  background: -webkit-linear-gradient(#272727, #8e8e8e92);
+  font-family: 'Poppins', sans-serif;
+  justify-content: center;
+  text-decoration: none;
+  border-radius: 100px;
+  align-items: center;
+  font-weight: bold;
+  font-weight: 300;
+  font-size: 14px;
+  margin: 0 auto;
+  color: #fff;
+  display: flex;
+  width: 180px;
+  height: 45px;
+  margin-top: -20px;
+}
+
+span{
+  color: #bdfb60;
+  font-size: 14px;
+}
+
+#sound{
+  margin-top: -20px;
+  cursor: pointer;
+  font-size: 30px;
+  color: #fff;
+}
+
+.events{
+  margin: 0 auto;
+  display: flex;
+  height: 30px;
 }
 
 </style>
